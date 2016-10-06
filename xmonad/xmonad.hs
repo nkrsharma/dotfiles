f525980c7b29fdfd96ac69fb8fbe51e691a1370a
Naveen Kr. Sharma <naveenks@cs.washington.edu>
2016-10-06 10:49:51 -0700

    xmonad nits

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,18 +1,24 @@
 import XMonad
+import XMonad.Config.Desktop
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.UrgencyHook
+import XMonad.Layout.NoBorders
 import XMonad.Util.Run(spawnPipe)
 import System.IO
 
 main = do
   xmproc <- spawnPipe "/usr/bin/xmobar ~/.xmobarrc"
-  xmonad $ defaultConfig
-        { manageHook = manageDocks <+> manageHook defaultConfig
-        , layoutHook = avoidStruts  $  layoutHook defaultConfig
-        , logHook = dynamicLogWithPP xmobarPP
-                        { ppOutput = hPutStrLn xmproc
-                        , ppTitle = xmobarColor "green" "" . shorten 50
-                        }
-        , terminal = "urxvt"
-        , modMask = mod4Mask 
-        }
+  xmonad $ withUrgencyHook NoUrgencyHook myConfig
+    { manageHook = manageDocks <+> manageHook myConfig
+    , layoutHook = avoidStruts $ smartBorders $ layoutHook myConfig
+    , logHook = dynamicLogWithPP xmobarPP
+      { ppOutput = hPutStrLn xmproc
+      , ppTitle = xmobarColor "green" "" . shorten 50
+      }
+    }
+
+myConfig = defaultConfig
+  { terminal = "urxvtc"
+  , modMask = mod4Mask
+  }
