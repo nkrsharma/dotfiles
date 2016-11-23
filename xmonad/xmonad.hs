6fc723ff1cd90029668991af5559b1c34a43f59d
Naveen Kr. Sharma <naveenks@cs.washington.edu>
2016-11-22 19:28:11 -0800

    switching from xmobar to dzen

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,3 +1,6 @@
+import System.IO
+import System.Posix.Unistd
+
 import XMonad
 import XMonad.Config.Desktop
 import XMonad.Hooks.DynamicLog
@@ -5,25 +8,37 @@ import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.UrgencyHook
 import XMonad.Layout.NoBorders
 import XMonad.Util.Run
-import System.IO
 
 import qualified Data.Map as M
 
 main = do
-  xmproc <- spawnPipe "/usr/bin/xmobar ~/.xmobar.hs"
-  xmonad $ withUrgencyHook NoUrgencyHook myConfig
-    { manageHook = manageDocks <+> manageHook myConfig
-    , layoutHook = avoidStruts $ smartBorders $ layoutHook myConfig
-    , logHook = dynamicLogWithPP xmobarPP
-      { ppOutput = hPutStrLn xmproc
-      , ppTitle = xmobarColor "#859900" "" . shorten 72
-      }
+  host <- fmap nodeName getSystemID
+  dzenLeftBar  <- spawnPipe myXmonadBar
+  dzenRightBar <- spawnPipe myStatusBar
+  xmonad $ withUrgencyHook NoUrgencyHook defaultConfig
+    { terminal   = myTerminal
+    , modMask    = mod4Mask
+    , workspaces = myWorkspaces
+    , keys       = \c -> myKeys c `M.union` keys defaultConfig c
+    , manageHook = manageDocks <+> manageHook defaultConfig
+    , layoutHook = avoidStruts $ smartBorders $ layoutHook defaultConfig
+    , logHook    = myLogHook dzenLeftBar host
     }
 
-myConfig = defaultConfig
-  { terminal = "urxvtc"
-  , modMask = mod4Mask
-  , keys = \c -> myKeys c `M.union` keys defaultConfig c
+myDzenStyle = "-h 15 -bg '#002b36' -fn '-*-terminus-medium-r-normal-*-12-*-*-*-*-*-iso8859-*' -xs 1 -e 'onstart=lower'"
+myXmonadBar = "dzen2 -ta l -w 800 -x 0 -y 0 " ++ myDzenStyle
+myStatusBar = "killall conky; conky -c ~/.conkyrc | dzen2 -ta r -x 800 -y 0 " ++ myDzenStyle
+
+myTerminal = "urxvtx"
+myWorkspaces = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+
+myLogHook output host = dynamicLogWithPP $ defaultPP
+  { ppOutput  = hPutStrLn output
+  , ppTitle   = dzenColor "#859900" "" . shorten 72
+  , ppCurrent = dzenColor "yellow" "" . wrap "[" "]"
+  , ppVisible = wrap "(" ")"
+  , ppUrgent  = dzenColor "red" "yellow"
+  , ppOrder   = \(w:l:t:e) -> [" "++w,l,t]
   }
 
 myKeys XConfig { modMask = modM } = M.fromList $
@@ -43,3 +58,4 @@ myKeys XConfig { modMask = modM } = M.fromList $
 
   -- Audio control
   ]
+
